1a291751265db2464fd53c73b595229d765b1369
Gustav Behm <gustav@upvest.co>
2018-03-27 17:40:46 +0200

    Add negative tests for the block api

--- core/src/it/scala/co/upvest/arweave4s/api/v1/HighlevelSpec.scala
+++ core/src/it/scala/co/upvest/arweave4s/api/v1/HighlevelSpec.scala
@@ -3,16 +3,15 @@ package co.upvest.arweave4s.api.v1
 import com.softwaremill.sttp.{HttpURLConnectionBackend, TryHttpURLConnectionBackend}
 import co.upvest.arweave4s.adt.Block
 import co.upvest.arweave4s.api.{ApiTestUtil, highlevel}
-import org.scalatest.{WordSpec, Matchers}
+import org.scalatest.{WordSpec, Matchers, Inside}
 
 import cats.Id
 import cats.instances.try_._
 
-import scala.util.{Try, Success}
+import scala.util.{Try, Success, Failure}
 
-class HighlevelSpec extends WordSpec with Matchers {
+class HighlevelSpec extends WordSpec with Matchers with Inside {
   import ApiTestUtil._
-
   import highlevel._
 
   implicit val idConfig = Config[Id](
@@ -27,30 +26,66 @@ class HighlevelSpec extends WordSpec with Matchers {
 
   "Highlevel" should {
     "block" should {
-      import id._
-      import monadError._
-
-      val Some(invalidBlockId) = Block.IndepHash.fromEncoded(
+      val Some(invalidBlockHash) = Block.IndepHash.fromEncoded(
         "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
       )
+
+      val invalidBlockHeight = BigInt(Long.MaxValue)
 
       val Some(validBlock) = Block.IndepHash.fromEncoded(
         "cOplqTqwO-JiUUfiJcl-KbD_9qpt3TPVdojgz-QB5h0"
       )
 
-      "return the current block" in {
-        block.current[Id]() shouldBe a[Block]
-        block.current[Try]() shouldBe a[Success[_]]
+      "using Id functor" should {
+        import id._
+
+        "return the current block" in {
+          block.current[Id]() shouldBe a[Block]
+        }
+
+        "return a valid block by hash" in {
+          block.get[Id](validBlock) shouldBe a[Block]
+        }
+
+        "return a valid block by height" in {
+          block.get[Id](BigInt(100)) shouldBe a[Block]
+        }
+
+        "fail when a block does not exist (by hash)" in {
+          assertThrows[HttpFailure] { block.get[Id](invalidBlockHash) }
+        }
+
+        "fail when a block does not exist (by height)" in {
+          assertThrows[HttpFailure] { block.get[Id](invalidBlockHeight) }
+        }
       }
 
-      "return a valid block by hash" in {
-        block.get[Id](validBlock) shouldBe a[Block]
-        block.get[Try](validBlock) shouldBe a[Success[_]]
-      }
+      "using Try" should {
+        import monadError._
 
-      "return a valid block by height" in {
-        block.get[Id](BigInt(100)) shouldBe a[Block]
-        block.get[Try](BigInt(100)) shouldBe a[Success[_]]
+        "return the current block" in {
+          block.current[Try]() shouldBe a[Success[_]]
+        }
+
+        "return a valid block by hash" in {
+          block.get[Try](validBlock) shouldBe a[Success[_]]
+        }
+
+        "return a valid block by height" in {
+          block.get[Try](BigInt(100)) shouldBe a[Success[_]]
+        }
+
+        "fail when a block does not exist (by hash)" in {
+          inside(block.get[Try](invalidBlockHash)) {
+            case Failure(HttpFailure(rsp)) => rsp.code shouldBe 404
+          }
+        }
+
+        "fail when a block does not exist (by height)" in {
+          inside(block.get[Try](invalidBlockHeight)) {
+            case Failure(HttpFailure(rsp)) => rsp.code shouldBe 404
+          }
+        }
       }
     }
   }
