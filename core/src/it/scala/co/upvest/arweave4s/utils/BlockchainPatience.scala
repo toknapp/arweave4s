793ba1a6a537945a6a7fa5c7776a330f23c13ac3
Gustav Behm <gustav@upvest.co>
2019-02-14 17:57:34 +0100

    Third attempt at increasing the timeout

--- core/src/it/scala/co/upvest/arweave4s/utils/BlockchainPatience.scala
+++ core/src/it/scala/co/upvest/arweave4s/utils/BlockchainPatience.scala
@@ -6,7 +6,7 @@ import org.scalatest.time.SpanSugar
 trait BlockchainPatience extends AbstractPatienceConfiguration {
   import SpanSugar._
   implicit override val patienceConfig = PatienceConfig(
-    timeout = scaled(1 minute),
+    timeout = scaled(3 minute),
     interval = scaled(4 seconds)
   )
 }
